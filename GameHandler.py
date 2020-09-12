93cf86db23eb408b7815ddc157bab198532cfab3
jb <jean-baptiste.dupuy@epitech.eu>
2020-09-12 22:30:42 +0200

    added pygame as displayer in gamehandler

--- GameHandler.py
+++ GameHandler.py
@@ -1,7 +1,8 @@
 import time
 from collections import OrderedDict
 
-from CliHandler import CliHandler
+from Displayers.CliDisplay import CliDisplay
+from Displayers.PyGame.PyGameDisplay import PyGameDisplay
 from MapHandler import MapHandler
 from Entities import *
 from utils import *
@@ -17,11 +18,12 @@ class GameHandler(object):
         s.hud_infos["score"] = 0
         s.hud_infos["empty"] = None
         s.hud_infos["time"] = int(time.time() - s.start_time)
-        s.cli_handler = CliHandler(stdscr, fps=10)
+        # s.cli_handler = CliDisplay(stdscr, fps=10)
+        s.cli_handler = PyGameDisplay(fps=10)
 
     def launch(s):
         while 42:
-            s.handle_inputs(s.cli_handler.get_inputs())
+            s.handle_inputs()
             s.handle_ia()
             s.hud_infos["time"] = int(time.time() - s.start_time)
             s.cli_handler.draw(s.map_handler, s.hud_infos)
@@ -29,15 +31,14 @@ class GameHandler(object):
     def handle_ia(s):
         return
 
-    def handle_inputs(s, player_input):
-        if not player_input or player_input not in ['z', 'q', 's', 'd', '\u001b']:
-            return
+    def handle_inputs(s):
+        player_input = s.cli_handler.get_inputs()
+        if player_input == "EXIT": exit()
+        if not player_input: return
         delta = None
-        if player_input == '\u001b':
-            exit(0)
-        if player_input == 'z':   delta = Pos(-1, 0)
-        elif player_input == 's': delta = Pos(1, 0)
-        if player_input == 'q':   delta = Pos(0, -1)
-        elif player_input == 'd': delta = Pos(0, 1)
+        if player_input == 'UP':   delta = Pos(-1, 0)
+        elif player_input == 'DOWN': delta = Pos(1, 0)
+        if player_input == 'LEFT':   delta = Pos(0, -1)
+        elif player_input == 'RIGHT': delta = Pos(0, 1)
         if delta and s.map_handler.move_entity_relative(s.map_handler.player_pos, delta):
             s.map_handler.player_pos += delta
