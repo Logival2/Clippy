abc84f360e3bda6875b9ff89d1dbe14e6c879a9b
jb <jean-baptiste.dupuy@epitech.eu>
2020-09-13 18:10:14 +0200

    Improved resolution handling in pygame and map drawing offset by one fixed

--- Displayers/PyGame/PyGameDisplay.py
+++ Displayers/PyGame/PyGameDisplay.py
@@ -11,40 +11,33 @@ from Displayers.PyGame.pygame_defines import *
 
 
 class PyGameDisplay(object):
-    def __init__(s, fps=20, hud_width=20):
+    def __init__(s, fps=20, target_resolution=None, hud_width=10):
         ### Pygame related ###
         pygame.init()
-        s.screen_size = pygame.display.Info()
-        s.screen_size = Pos(x=1920, y=1080)
-        # s.screen_size = Pos(s.screen_size.current_h, s.screen_size.current_w)
-        s.square_size = 16  # Must be even
-        s.squares_nbr = s.screen_size // s.square_size
-        # print(s.squares_nbr)
+        s.square_size = 26  # Should be even
+        if target_resolution:
+            tmp_screen_size = target_resolution
+        else:
+            tmp_screen_size = pygame.display.Info()
+            tmp_screen_size = Pos(x=tmp_screen_size.current_w, y=tmp_screen_size.current_h)
+        s.squares_nbr =  tmp_screen_size // s.square_size
+        if s.squares_nbr.x % 2:
+            s.squares_nbr.x -=1
+        if s.squares_nbr.y % 2:
+            s.squares_nbr.y -=1
+        s.screen_size = s.squares_nbr * s.square_size
         s.hud_squares_nbr = hud_width
         s.map_squares_nbr = Pos(x=s.squares_nbr.x - 3 - s.hud_squares_nbr, y=s.squares_nbr.y - 2)
-        # ((0,0),pygame.FULLSCREEN)
         s.display = pygame.display.set_mode((s.screen_size.x , s.screen_size.y))
         pygame.display.set_caption('Name')
         ### FPS related ###
         s.delta = 1 / fps
         s.frame_start = time.time()
         ### FONTS ###
-        # Main font
         s.main_font = pygame.font.Font('Displayers/PyGame/fonts/CozetteVector.ttf', 32)
-        # Resize unicode font till it fits into a single square
-        font_start_size = 22
-        s.font = pygame.font.Font('Displayers/PyGame/fonts/Everson_Mono.ttf', font_start_size)
-        # x, y = s.font.size('x')
-        # while x > s.square_size or y > s.square_size:
-        #     s.font = pygame.font.Font('Displayers/PyGame/fonts/Everson_Mono.ttf', font_start_size)
-        #     x, y = s.font.size('x')
-        #     font_start_size -= 1
         ### IMAGES ###
         s.images = {}
         s.load_available_images()
-        # s.load_sprite('player')
-        # s.load_sprite('floor')
-        # s.load_sprite('grass')
 
     def __del__(s):
         pygame.quit()
@@ -53,13 +46,13 @@ class PyGameDisplay(object):
         s.display.fill(BLACK)
         s.draw_borders()
         s.draw_map(map_handler)
-        # s.draw_grid()
+        s.draw_grid()
         pygame.display.update()
         s.handle_sleep()
 
     def draw_map(s, map_handler):
         # Nbr of lines available from the player (-1) to the top of the map screen
-        needed_lines_top = s.map_squares_nbr.y // 2
+        needed_lines_top = s.map_squares_nbr.y // 2 + 2
         to_add_top = needed_lines_top - map_handler.player_pos.y
         term_y_idx = 0 if to_add_top <= 0 else to_add_top
         map_y_idx = 0 if to_add_top >= 0 else -to_add_top
@@ -68,16 +61,16 @@ class PyGameDisplay(object):
         squares_to_add_left = needed_squares_left - map_handler.player_pos.x
         shift_x = 0 if squares_to_add_left <= 0 else squares_to_add_left
         map_x_start = 0 if squares_to_add_left >= 0 else -squares_to_add_left
-
-        avail_squares_right = s.map_squares_nbr.x // 2 + 2
+        shift_x += 1
+        avail_squares_right = s.map_squares_nbr.x // 2 + 1
         map_x_end = map_handler.player_pos.x + avail_squares_right
 
         final_player_pos = Pos(
                             term_y_idx + map_handler.player_pos.y - map_y_idx,
                             shift_x + (map_handler.player_pos.x - map_x_start) * 2
                         )
-
-        while term_y_idx < s.map_squares_nbr.y + 1:
+        while term_y_idx < s.map_squares_nbr.y:
+            term_y_idx += 1
             try:
                 tmp_line = map_handler.map[map_y_idx][map_x_start:map_x_end]
             except IndexError:
@@ -86,20 +79,6 @@ class PyGameDisplay(object):
                 if square:
                     types = square.get_types()
                     s.display_entity(types, Pos(x=shift_x + x_idx, y=term_y_idx))
-                    # name = 'floor'
-                    # color = WHITE
-                    # if char == '⧱ ':
-                    #     color = GREEN
-                    #     name = 'player'
-                    # elif char == 'w':
-                    #     name = 'wall'
-                    # text_surface = s.font.render(char[1], False, color)
-                    # # s.display.blit(text_surface, s.get_square_px_pos(Pos(x=shift_x + x_idx, y=term_y_idx)).get_tuple())
-                    # s.display.blit(
-                    #             s.images[name],
-                    #             s.get_square_px_pos(Pos(x=shift_x + x_idx, y=term_y_idx)).get_tuple()
-                    #         )
-            term_y_idx += 1
             map_y_idx += 1
 
     def display_entity(s, ent_types, pos):
@@ -184,9 +163,11 @@ class PyGameDisplay(object):
     def load_available_images(s):
         images_dir_path = 'Displayers/PyGame/images/'
         images_names_list = [f[:f.find('.')] for f in listdir(images_dir_path) if isfile(join(images_dir_path, f))]
-        print(f"loading textures:\n{images_names_list}")
+        print(f"[+] Loading textures: {', '.join(images_names_list)}")
         for image_name in images_names_list:
-            s.images[image_name] = pygame.image.load(f'Displayers/PyGame/images/{image_name}.png')
+            tmp_img = pygame.image.load(f'Displayers/PyGame/images/{image_name}.png')
+            tmp_img = pygame.transform.scale(tmp_img, (s.square_size, s.square_size))
+            s.images[image_name] = tmp_img
 
     def handle_sleep(s):
         to_sleep = s.delta - (time.time() - s.frame_start)
