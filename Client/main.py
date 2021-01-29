260071a64da142aedcfb10dc713c2fc5f82056f1
jb <jean-baptiste.dupuy@epitech.eu>
2021-01-29 20:51:00 +0100

    client can now receive the map

--- Client/main.py
+++ Client/main.py
@@ -10,47 +10,49 @@ from GameLoop import GameLoop
 from config import *
 
 
-class ChaseClient(Client):
-    '''Subclass pygase classes to scope event handlers and game-specific variables.
-    '''
+class NetworkClient(Client):
+    '''Subclass pygase classes to scope event handlers and game-specific variables.'''
     def __init__(self):
         super().__init__()
         self.player_id = None
+        self.map = None
         # The backend will send a "PLAYER_CREATED" event in response to a "JOIN" event.
         self.register_event_handler("PLAYER_CREATED", self.on_player_created)
 
-    def on_player_created(self, player_id):
+    def on_player_created(self, player_id, map):
         '''"PLAYER_CREATED" event handler'''
         # Remember the id the backend assigned the player.
         self.player_id = player_id
-
+        self.map = map
 
 class App(object):
-    def __init__(s):
-        s.client = ChaseClient()
-        s.game_loop = GameLoop(DISPLAY_CONFIG, s.client)
+    def __init__(self):
+        self.client = NetworkClient()
+        self.game_loop = GameLoop(DISPLAY_CONFIG, self.client)
         # Final window size is needed as the window size
         # will shrink to adapt to the tile size specified in config.py
-        s.main_menu_loop = MainMenuLoop(s.game_loop.screen_size, s.client)
-        s.active_loop = s.main_menu_loop
-        s.clock = pygame.time.Clock()
+        self.main_menu_loop = MainMenuLoop(self.game_loop.screen_size, self.client)
+        self.active_loop = self.main_menu_loop
+        self.clock = pygame.time.Clock()
 
-    def __del__(s):
-        if s.client.connection is not None:
-            s.client.disconnect()
+    def __del__(self):
+        if self.client.connection is not None:
+            self.client.disconnect()
 
-    def run(s):
+    def run(self):
         while True:
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
-                    if s.client.connection is not None:
-                        s.client.disconnect()
+                    if self.client.connection is not None:
+                        self.client.disconnect()
                     exit()
-            if not s.active_loop.update(events, s.game_loop.display):
-                s.active_loop = s.game_loop
+            if not self.active_loop.update(events, self.game_loop.display):
+                self.active_loop = self.game_loop
+                self.map = self.client.map
+                pprint(self.map)
             pygame.display.update()
-            s.clock.tick(60)
+            self.clock.tick(60)
 
 
 if __name__ == '__main__':
