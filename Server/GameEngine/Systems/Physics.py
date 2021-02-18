dbadf4ab0a68c3abab3936bb60e8ffec2c58273b
Seb-lg <seb.le.gall@free.fr>
2021-02-18 19:11:50 +0100

    fix physic issue

--- Server/GameEngine/Systems/Physics.py
+++ Server/GameEngine/Systems/Physics.py
@@ -4,6 +4,7 @@ from GameEngine.Components.Hitbox import Hitbox
 from GameEngine.Components.Position import Position
 from GameEngine.GameServer import ecs
 
+
 def does_collide(pos, ids, positions):
     for collide in ids:
         if pos == positions[collide] and id != collide:
@@ -21,10 +22,14 @@ def hitbox_update():
     for id in non_static:
         for collide in collides:
             if positions[id] == positions[collide] and id != collide:
-                if not does_collide(Position(x=hitboxs[id].x, y=positions[id].y), ids, positions):
+                if not does_collide(Position(x=hitboxs[id].x, y=positions[id].y), collides, positions):
                     positions[id].x = hitboxs[id].x
-                elif not does_collide(Position(x=positions[id].x, y=hitboxs[id].y), ids, positions):
+                elif not does_collide(Position(x=positions[id].x, y=hitboxs[id].y), collides, positions):
                     positions[id].y = hitboxs[id].y
+                else:
+                    positions[id].x = hitboxs[id].x
+                    positions[id].y = hitboxs[id].y
+
         collides = collides[1:]
     for id in ids:
         hitboxs[id].x = positions[id].x
